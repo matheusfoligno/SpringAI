6c41dbfffd9f62d3347829435581565749877f3a
Matheus <matheusfoligno@gmail.com>
2025-11-25 09:23:25 -0300

    feat: using prompt templates

---
+++ springai/src/main/java/com/matheus/springai/configs/ChatClientConfig.java
@@ -0,0 +1,23 @@
+package com.matheus.springai.configs;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChatClientConfig {
+
+    @Bean
+    public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
+        return chatClientBuilder
+                .defaultSystem("""
+                        You are an internal IT helpdesk assistant. Your role is to assist 
+                        employees with IT-related issues such as resetting passwords, 
+                        unlocking accounts, and answering questions related to IT policies.
+                        If a user requests help with anything outside of these 
+                        responsibilities, respond politely and inform them that you are 
+                        only able to assist with IT support tasks within your defined scope.
+                        """)
+                .build();
+    }
+}
--- springai/src/main/java/com/matheus/springai/controllers/ChatController.java
+++ springai/src/main/java/com/matheus/springai/controllers/ChatController.java
@@ -12,22 +12,14 @@ public class ChatController {
 
 	private final ChatClient chatClient;
 	
-	public ChatController(ChatClient.Builder chatClientBuilder) {
-		this.chatClient = chatClientBuilder.build();
+	public ChatController(ChatClient chatClient) {
+		this.chatClient = chatClient;
 	}
 
 	@GetMapping("/chat")
 	public String chat(@RequestParam String message) {
 		return chatClient
                 .prompt()
-                .system("""
-                        You are an internal IT helpdesk assistant. Your role is to assist 
-                        employees with IT-related issues such as resetting passwords, 
-                        unlocking accounts, and answering questions related to IT policies.
-                        If a user requests help with anything outside of these 
-                        responsibilities, respond politely and inform them that you are 
-                        only able to assist with IT support tasks within your defined scope.
-                        """)
                 .user(message)
                 .call().content();
 	}
