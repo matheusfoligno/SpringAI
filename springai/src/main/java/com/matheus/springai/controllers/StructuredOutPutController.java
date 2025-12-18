71f453dc97ac5a19ba6ff6121dbf892c39e4f9f6
Matheus <matheusfoligno@gmail.com>
2025-12-18 20:04:15 -0300

    feat: map and list output converter examples

--- springai/src/main/java/com/matheus/springai/controllers/StructuredOutPutController.java
+++ springai/src/main/java/com/matheus/springai/controllers/StructuredOutPutController.java
@@ -2,11 +2,16 @@ package com.matheus.springai.controllers;
 
 import com.matheus.springai.models.CountryCities;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.converter.ListOutputConverter;
+import org.springframework.ai.converter.MapOutputConverter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -24,6 +29,26 @@ public class StructuredOutPutController {
                 .prompt()
                 .user(message)
                 .call().entity(CountryCities.class);
+
+        return ResponseEntity.ok(countryCities);
+    }
+
+    @GetMapping("/chat-list")
+    public ResponseEntity<List<String>> chatList(@RequestParam String message) {
+        List<String> countryCities = chatClient
+                .prompt()
+                .user(message)
+                .call().entity(new ListOutputConverter());
+
+        return ResponseEntity.ok(countryCities);
+    }
+
+    @GetMapping("/chat-map")
+    public ResponseEntity<Map<String, Object>> chatMap(@RequestParam String message) {
+        Map<String, Object> countryCities = chatClient
+                .prompt()
+                .user(message)
+                .call().entity(new MapOutputConverter());
 
         return ResponseEntity.ok(countryCities);
     }
