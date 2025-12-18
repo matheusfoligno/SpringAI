51c99f8dbd86e85b5210e7a5504353dcb437de2a
Matheus <matheusfoligno@gmail.com>
2025-12-18 20:17:42 -0300

    feat: using parameterizedtypereference

--- springai/src/main/java/com/matheus/springai/controllers/StructuredOutPutController.java
+++ springai/src/main/java/com/matheus/springai/controllers/StructuredOutPutController.java
@@ -4,6 +4,7 @@ import com.matheus.springai.models.CountryCities;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.converter.ListOutputConverter;
 import org.springframework.ai.converter.MapOutputConverter;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,6 +50,16 @@ public class StructuredOutPutController {
                 .prompt()
                 .user(message)
                 .call().entity(new MapOutputConverter());
+
+        return ResponseEntity.ok(countryCities);
+    }
+
+    @GetMapping("/chat-bean-list")
+    public ResponseEntity<List<CountryCities>> chatBeanList(@RequestParam String message) {
+        List<CountryCities> countryCities = chatClient
+                .prompt()
+                .user(message)
+                .call().entity(new ParameterizedTypeReference<List<CountryCities>>() {});
 
         return ResponseEntity.ok(countryCities);
     }
