f5227017cf76749fc387a438a0a6796138e7e486
Matheus <matheusfoligno@gmail.com>
2026-01-29 08:54:39 -0300

    feat: setting up rag example

---
+++ spring-ai-rag/src/test/java/com/matheus/springairag/SpringairagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.springairag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringairagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
