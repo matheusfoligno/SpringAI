3a71fc3fb2096d46ca71843c983256a6ca0b5f51
Matheus <matheusfoligno@gmail.com>
2026-02-22 15:04:09 -0300

    Mcp server using stdio

---
+++ mcpserverstdio/src/main/java/com/matheus/mcpserverstdio/models/TicketRequest.java
@@ -0,0 +1,3 @@
+package com.matheus.mcpserverstdio.models;
+
+public record TicketRequest(String issue, String username) { }
---
+++ mcpserverstdio/src/main/java/com/matheus/mcpserverstdio/repositories/HelpDeskTicketRepository.java
@@ -0,0 +1,12 @@
+package com.matheus.mcpserverstdio.repositories;
+
+import com.matheus.mcpserverstdio.entities.HelpDeskTicket;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface HelpDeskTicketRepository extends JpaRepository<HelpDeskTicket, Long> {
+
+    List<HelpDeskTicket> findByUsername(String username);
+
+}
